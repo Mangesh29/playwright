7b0722e157fd0cef72161afa79695b1b752087d1
Mangesh29 <bhosalemangesh260@gmail.com>
2025-12-12 17:00:50 +0530

    exercise work

--- exercise.py
+++ exercise.py
@@ -12,3 +12,13 @@ favorite_cool="Blue"
 print(type(age))
 print(type(height))
 print(type(favorite_cool))
+
+
+
+
+
+fruits=["apple","banana","cherry","date","elderberry"]
+
+print(fruits[0])
+print(fruits[4])
+print(fruits[1:3])
