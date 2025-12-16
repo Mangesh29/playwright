3a3675b939ea46b15200576750176797cd0f1984
Mangesh29 <bhosalemangesh260@gmail.com>
2025-12-16 15:34:30 +0530

    Test case use for fixture

--- Pytestcase/test2_validation.py
+++ Pytestcase/test2_validation.py
@@ -1,3 +1,6 @@
+import pytest
 
+
+@pytest.mark.smoke
 def test_thirdCheck(preSetupWork):
     print("This is third test case")  
--- Pytestcase/test_validation.py
+++ Pytestcase/test_validation.py
@@ -3,15 +3,23 @@ import pytest
 
 @pytest.fixture(scope ="module") # scope can be function, class , module , session
 def preWork():
-    print(" I setup browser insatnace")
+    print(" I setup browser insatnace") # pre condition code
+    return "Fail" # post condition code
 
 
+@pytest.fixture(scope ="function") # scope can be function, class , module , session
+def secondWork():
+    print(" I setup secondwork insatnace") # pre condition code
+    yield  
+    print("tear down validation")
 
-
-def test_initialCheck(preWork):
+@pytest.mark.smoke
+def test_initialCheck(preWork, secondWork):
     print("This is frist test case")
+    assert preWork =="Fail"
 
 
-def test_SecondCheck(preSetupWork):
+@pytest.mark.skip #skipping test case
+def test_SecondCheck(preSetupWork, secondWork):
     print("This is second test case")  
-    
+ 
